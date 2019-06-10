3a6c5f65a0865ec9de3076110f0d3bb9ebcf69b8
MSC <msc@mscs-mbp.br.ibm.com>
2019-06-10 09:48:03 -0300

    ajustes msg error

--- ngfront/src/app/app.component.ts
+++ ngfront/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  
+
   title = 'ngfront';
+  success_msg = '';
+  error_msg = '';
+  error = '';
+
+
+
 }
